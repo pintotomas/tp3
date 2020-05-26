01071e672b5408b7b4faf56aa9b5e1386c791ba3
pintotomas <tomasp834@gmail.com>
2020-05-25 22:47:20 -0300

    Fixes

--- server_HelpCommand.cpp
+++ server_HelpCommand.cpp
@@ -5,9 +5,9 @@ HelpCommand::HelpCommand() {}
 HelpCommand::~HelpCommand() {}
 
 std::string HelpCommand::get_response(GuessNumberGame &game) {
-	std::string response = "Comandos válidos: \n\t AYUDA: despliega la lista de ";
-	response = response + "comandos válidos \n\t RENDIRSE: pierde";
-	response = response + " el juego automáticamente \n\t XXX: Número de";
+	std::string response = "Comandos válidos: \n\tAYUDA: despliega la lista de ";
+	response = response + "comandos válidos \n\tRENDIRSE: pierde";
+	response = response + " el juego automáticamente \n\tXXX: Número de";
 	response = response + " 3 cifras a ser enviado al servidor para adivinar";
 	response = response + " el número secreto";
 	response_size = response.length();
